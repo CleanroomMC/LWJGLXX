175932bc58cbecc449b3afd0551ccec01e1aa018
arcade_kappa <gkappa@outlands.top>
2024-12-19 17:35:17 +0800

    First commit

---
+++ src/main/java/org/lwjglx/opengl/ARBTextureBarrier.java
@@ -0,0 +1,8 @@
+package org.lwjglx.opengl;
+
+public class ARBTextureBarrier {
+
+    public static void glTextureBarrier() {
+        org.lwjgl3.opengl.ARBTextureBarrier.glTextureBarrier();
+    }
+}
